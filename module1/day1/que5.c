8fffe5a38bf01b92939d8e55f26d2f8a2d4e2272
HARSHAVARDHANREDDYTHALAKOLA <135808859+HARSHAVARDHANREDDYTHALAKOLA@users.noreply.github.com>
2023-06-16 04:10:58 +0000

    commit1 : adding que5.c : main

--- module1/day1/que5.c
+++ module1/day1/que5.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 
 int bit_operations(int num, int oper_type) {
-    int result = num;
-
-    if (oper_type == 1) {
-        result |= 1; // Set 1st bit
-    } else if (oper_type == 2) {
-        result &= ~(1 << 31); // Clear 31st bit
-    } else if (oper_type == 3) {
-        result ^= (1 << 16); // Toggle 16th bit
-    } else {
-        printf("Invalid operation type.\n");
-        return num; // Return original number if operation type is invalid
+    switch (oper_type) {
+        case 1:  // Set 1st bit
+            num |= (1 << 0);
+            break;
+        case 2:  // Clear 31st bit
+            num &= ~(1 << 31);
+            break;
+        case 3:  // Toggle 16th bit
+            num ^= (1 << 15);
+            break;
+        default:
+            printf("Invalid operation type\n");
+            break;
     }
 
-    return result;
+    return num;
 }
 
 int main() {
     int num, oper_type;
+
     printf("Enter an integer (32 bits): ");
     scanf("%d", &num);
-    
+
     printf("Enter operation type (1, 2, or 3): ");
     scanf("%d", &oper_type);
-    
+
     int result = bit_operations(num, oper_type);
+
     printf("Result: %d\n", result);
-    
+
     return 0;
 }
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
