b92917c831048411023d760706e78c04050ebcf3
HARSHAVARDHANREDDYTHALAKOLA <135808859+HARSHAVARDHANREDDYTHALAKOLA@users.noreply.github.com>
2023-06-16 12:08:32 +0000

    commit1 : adding que1.c : main

--- module1/day2/que1.c
+++ module1/day2/que1.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-void printExponent(double num) {
-    unsigned long long* ptr = (unsigned long long*)&num;
+void printExponent(double x) {
+    unsigned long long *ptr = (unsigned long long *)&x;
     unsigned long long exponent = (*ptr >> 52) & 0x7FF;
 
-    printf("Exponent in hexadecimal: 0x%llX\n", exponent);
-    printf("Exponent in binary: 0b");
+    // Print hexadecimal format
+    printf("Hexadecimal: 0x%llX\n", exponent);
+
+    // Print binary format
+    printf("Binary: 0b");
     for (int i = 10; i >= 0; i--) {
-        printf("%lld", (exponent >> i) & 1);
+        printf("%d", (exponent >> i) & 1);
     }
     printf("\n");
 }
